3c3f87d247ae6dd29bf53b6fa1e0c12be2ae4394
Yusuf Bilal <ubahyusuf484@gmail.com>
2022-04-30 23:23:49 +0100

    i no even sure again sef

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ const removeLoader = () => {
 }
 
 window.onload = () => {
-    setTimeout(removeLoader, 4000)
+    removeLoader()
     AOS.init({ once: true, duration: 1500, delay: 300 });
     tlChangeText.play();
 }
@@ -63,61 +63,108 @@ const tlChangeText = new TimelineLite({
         },
         3), 2.5)
 
+//expand se
+
 
 //card displays subcategories on hover
 const categoryCard = document.querySelectorAll('.category__card');
 
 categoryCard.forEach((card) => {
-    const hoverCard = gsap.timeline({ paused: true, ease: "Power1.easeIn" })
+    const hoverCardTl = gsap.timeline({ paused: true, ease: "Power1.easeIn" })
         .addLabel('hoverCard')
-        .to(card.querySelector('.card__overlay'), { y: '-50%', duration: .9 }, 'hoverCard') //card moves up
+        .to(card.querySelector('.card__overlay'), { y: '-50%', translateY: '50%', duration: .9 }, 'hoverCard') //card moves up
         .fromTo(card.querySelector('img'), .7, { scale: 1, filter: 'brightness(1)' }, { scale: 1.5, filter: 'brightness(0.6)' }, 'hoverCard') //image zooms in
         .to(card.querySelector('.overlay__hidden'), { height: 'auto', opacity: .7, duration: .9 }, 'hoverCard+=.1') //hidden display is made visible
 
     card.addEventListener('mouseenter', () => {
         if (window.innerWidth > 425) {
-            hoverCard.play();
+            hoverCardTl.play();
         }
     })
 
     card.addEventListener('mouseleave', () => {
         if (window.innerWidth > 425) {
-            hoverCard.reverse();
+            hoverCardTl.reverse();
         }
     })
+
+    //scrolltrigger overlay (for smaller screens)
+    if (('ontouchstart' in document.documentElement && /mobi/i.test(navigator.userAgent))) {
+        ScrollTrigger.create({
+            trigger: card,
+            start: "top-=20% 20%",
+            onEnter: () => hoverCardTl.play(),
+            onLeave: () => hoverCardTl.reverse()
+        })
+    }
 })
 
 //remove label from product on click
 const productImages = document.querySelectorAll('.product-image');
 
 productImages.forEach((productImage) => {
-    const imgSwitch = gsap.timeline({ paused: true })
-        .set(productImage.querySelector('.product-image__second'), { display: 'block' })
-        .set(productImage.querySelector('.product-image__first'), { display: 'none' })
-        .set(productImage.parentElement.querySelector('.label-overlay'), { display: 'none' })
-        .set(productImage.querySelector('.image__hidden-display'), { display: 'flex' })
-        .from(productImage.querySelector('.image__hidden-display'), { duration: .4, autoAlpha: 0, y: '-1.2rem' })
+    const imgSwitchTl = gsap.timeline({ paused: true })
+        .addLabel('start')
+        .set(productImage.querySelector('.product-image__second'), { autoAlpha: 1 }, 'start+=.25')
+        .set(productImage.querySelector('.product-image__first'), { autoAlpha: 0 }, 'start+=.25')
+        .to(productImage.querySelector('.image__hidden-display'), { duration: .5, autoAlpha: 1 }, 'start')
+        .from(productImage.querySelector('.image__hidden-display'), { duration: .5, top: '60%' }, 'start')
 
     productImage.addEventListener('mouseenter', () => {
         console.log('product');
-        imgSwitch.play();
+        imgSwitchTl.play();
     })
 
     productImage.addEventListener('mouseleave', () => {
-        imgSwitch.reverse();
+        imgSwitchTl.reverse();
     })
+
+    //scrolltrigger overlay (for smaller screens)
+    if (('ontouchstart' in document.documentElement && /mobi/i.test(navigator.userAgent))) {
+        ScrollTrigger.create({
+            trigger: productImage,
+            start: "top-=20% 20%",
+            onEnter: () => imgSwitchTl.play(),
+            onLeave: () => imgSwitchTl.reverse()
+        })
+    }
 })
 
 
-var collapsed = true;
-function navbarToggle() {
-    var smd = document.querySelectorAll(".navbar__smd");
+//toggle navbar
+const navbarToggler = document.querySelector('#navbar-toggler');
+let collapsed = true;
+let smd = document.querySelectorAll(".navbar__smd");
+const collapseNavTl = gsap.timeline({ paused: true }).addLabel('start')
+    .to(smd, .75, { css: { height: 'auto', overflow: 'visible', marginBottom: 25 }, ease: Power1.easeInOut }, 'start')
+    .set(smd, { autoAlpha: 1 }, 'start+=.8')
 
+navbarToggler.addEventListener('click', navbarToggle);
+
+function navbarToggle() {
     if (collapsed) {
-        gsap.to(smd, { height: 'auto', overflow: 'visible', marginBottom: '1rem' });
+        collapseNavTl.play()
     } else {
-        gsap.to(smd, { height: 0, overflow: 'hidden',  marginBottom: '0rem' });
+        collapseNavTl.reverse();
     }
 
     collapsed = !collapsed;
 }
+
+
+// display blog posts
+const cardBlog = document.querySelectorAll('.card__blog');
+
+cardBlog.forEach((blog) => {
+    const blogOverlayTl = gsap.timeline({ paused: true })
+        .to(blog.querySelector('.blog-overlay'), .25, { autoAlpha: 1 })
+
+    blog.addEventListener('mouseenter', () => {
+        blogOverlayTl.play();
+    })
+
+    blog.addEventListener('mouseleave', () => {
+        blogOverlayTl.reverse();
+    })
+
+})
